24647962f028bfdacd4e517bed6c6383e9cd7d85
zednis <lokipatera@gmail.com>
2015-06-02 13:48:34 -0600

    fixed classname typo

--- productMods/js/siteAdmin/updateEditForm.js
+++ productMods/js/siteAdmin/updateEditForm.js
@@ -2,12 +2,12 @@
  * Created by szednik on 5/22/15.
  */
 
-function updateEditForm(dropdown){
+function updateEditForm(dropdown) {
 
     var newIndividualFormGenerator = "edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.NewIndividualFormGenerator";
 
     var dcatDatasetURI = "http://www.w3.org/ns/dcat#Dataset";
-    var newDatasetFormGenerator = "edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.newDatasetFormGenerator";
+    var newDatasetFormGenerator = "edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.NewDatasetFormGenerator";
 
     if(dropdown.value == dcatDatasetURI) {
         document.getElementById("editForm").value = newDatasetFormGenerator;
